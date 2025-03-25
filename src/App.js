cf9de0a6e7e4821e6d847f36fa153b7e95416e00
Zhiqin He <32912199+Zhiqin-He@users.noreply.github.com>
2025-03-25 03:31:16 -0400

    Create deploy.yml

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './app.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import AboutMe from './components/About Me/AboutMe';
-import Projects from './components/Projects/Project';
+// import Projects from './components/Projects/Project';
 import {useSelector} from 'react-redux';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import {Route, Switch, Redirect, useLocation} from "react-router-dom";
+// import {CSSTransition, TransitionGroup} from 'react-transition-group';
+// import {Route, Switch, Redirect, useLocation} from "react-router-dom";
 
 const App = () => {
     // Accessing the theme from the Redux store
@@ -26,32 +26,6 @@ const App = () => {
             {/*<Mode/>*/}
         </div>
     );
-
-    /* Uncomment the following code block to enable routing with React Router */
-    // const location = useLocation();
-    // return (
-    //     <div className="app-content">
-    //         <TransitionGroup>
-    //             <CSSTransition timeout={300} classNames="fade" key={location.key}>
-    //                 <Switch location={location}>
-    //                     <Route path="/" exact>
-    //                         <Redirect to="/home"/>
-    //                     </Route>
-    //                     <Route path="/home" component={Home}/>
-    //                     <Route path="/about-me" component={AboutMe}/>
-    //                     <Route path="/projects" component={Projects}/>
-    //                     <Route path="/skills" component={Education}/>
-    //                     <Route path="/experience" component={Education}/>
-    //                     <Route path="/contact" component={Education}/>
-    //                     <Route path="*">
-    //                         <Redirect to="/home"/>
-    //                     </Route>
-    //                 </Switch>
-    //             </CSSTransition>
-    //         </TransitionGroup>
-    //         <Footer/>
-    //     </div>
-    // );
 };
 
 export default App;

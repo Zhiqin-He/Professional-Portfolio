612fc53d8b003323fd5558e99a1cf0ecc025fc31
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2025-03-25 03:15:21 -0400

    Home + about me

--- src/Data/SocialData.js
+++ src/Data/SocialData.js
@@ -1,14 +1,10 @@
-const githubLink="https://github.com/killer-abhi";
-const instaLink="https://instagram.com/killer._abhi";
-const linkedInLink="https://www.linkedin.com/in/abhishek-kumar-1a295217b/";
-const twitterLink="https://twitter.com/ABHIDK0914";
-const emailLink="mailto:dk9554654249@gmail.com";
+const githubLink="https://github.com/Zhiqin-He";
+const linkedInLink="https://www.linkedin.com/in/zhiqin-he-90402a22a/";
+const emailLink="mailto:zhiqinhe159293@gmail.com";
 
 const profileLinks={
     githubLink,
-    instaLink,
     linkedInLink,
-    twitterLink,
     emailLink
 }
 const SocialData=profileLinks;
